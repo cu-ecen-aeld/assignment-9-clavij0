16a05a29957f585fd0ee7bdbee63f52e17056916
Jorge Clavijo <clavijo.jorge@outlook.com>
2024-05-05 19:10:19 -0500

    commit-6

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -14,7 +14,7 @@ AESD_ASSIGNMENTS_VERSION = '9d2af6a'
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-clavij0.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
-   
+    
  
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
