1e7e42ecdd3e3ea035560f246f4f6d4621a7ed82
Jorge Clavijo <clavijo.jorge@outlook.com>
2025-01-21 17:18:29 -0500

    change on AESDCHAR_DRIVER_VERSION v2

--- base_external/package/aesdchar-driver/aesdchar-driver.mk
+++ base_external/package/aesdchar-driver/aesdchar-driver.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 
-AESDCHAR_DRIVER_VERSION = 'f7175f2'
+AESDCHAR_DRIVER_VERSION = 'dfcf062'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
