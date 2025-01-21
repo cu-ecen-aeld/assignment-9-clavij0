bf6c8b0ebeddd2adc07f9d61d67f0047c36d4be2
Jorge Clavijo <clavijo.jorge@outlook.com>
2025-01-21 18:11:28 -0500

    Change on AESD_ASSIGNMENTS_VERSION

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,18 +6,13 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-#AESD_ASSIGNMENTS_VERSION = 'e281680'							
-#AESD_ASSIGNMENTS_VERSION = '9c1f154'
-AESD_ASSIGNMENTS_VERSION = 'a5f7019'
-#AESD_ASSIGNMENTS_VERSION = '5410e988415738bffd16c3b27ef4bafea8279d73'
+AESD_ASSIGNMENTS_VERSION = 'ed6b360'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE ='https://github.com/cu-ecen-aeld/assignments-3-and-later-clavij0.git'
-#AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-clavij0.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
-#AESD_ASSIGNMENTS_OVERRIDE_SRCDIR=/home/clavijo/Documents/Coursera/LinuxSPIBuildroot/finder-app
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
@@ -37,7 +32,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/Makefile $(TARGET_DIR)/bin
 
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/bin	
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin	
 	$(INSTALL) -m 0755 $(@D)/server/startup-aesdsocket.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 
 endef
